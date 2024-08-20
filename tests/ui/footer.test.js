0377d20b76ece6352be0583d3f3f08c0f193fffb
Georgi Ivanov <Georgi_Ivanov92@pm.me>
2024-08-20 17:02:44 +0200

    fixed footer test to be more exact

--- tests/ui/footer.test.js
+++ tests/ui/footer.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
-test('Check footer', async ({ page }) => {
+test.only('Check footer', async ({ page }) => {
     await page.goto('http://localhost:8080');
-    const footer = await page.$('div');
+    const footer = await page.$('footer > div');
     const text = await footer.textContent();
     expect(text).toContain(
         '© 2023 - Software Engineering and DevOps regular exam'
